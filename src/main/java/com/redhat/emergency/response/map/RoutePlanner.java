c27e3a92b1a8e3bd8cf34b4912ea60c13af46292
Bernard Tison <bernard.tison@gmail.com>
2020-06-30 01:01:12 +0200

    RoutePlanner implementation and test

--- src/main/java/com/redhat/emergency/response/map/RoutePlanner.java
+++ src/main/java/com/redhat/emergency/response/map/RoutePlanner.java
@@ -1,18 +1,79 @@
 package com.redhat.emergency.response.map;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.MapboxDirections;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.RouteLeg;
+import com.mapbox.core.constants.Constants;
+import com.mapbox.geojson.Point;
 import com.redhat.emergency.response.model.Location;
 import com.redhat.emergency.response.model.MissionStep;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit2.Response;
 
 @ApplicationScoped
 public class RoutePlanner {
 
+    private static final Logger log = LoggerFactory.getLogger(RoutePlanner.class);
+
+    @ConfigProperty(name = "mapbox.token")
+    String accessToken;
+
+    @ConfigProperty(name = "mapbox.url", defaultValue = Constants.BASE_API_URL)
+    String mapboxUrl;
+
     public List<MissionStep> getDirections(Location origin, Location destination, Location waypoint) {
 
-        return new ArrayList<>();
+        MapboxDirections request = MapboxDirections.builder()
+                .baseUrl(mapboxUrl)
+                .accessToken(accessToken)
+                .origin(Point.fromLngLat(origin.getLongitude().doubleValue(), origin.getLatitude().doubleValue()))
+                .destination(Point.fromLngLat(destination.getLongitude().doubleValue(), destination.getLatitude().doubleValue()))
+                .addWaypoint(Point.fromLngLat(waypoint.getLongitude().doubleValue(), waypoint.getLatitude().doubleValue()))
+                .overview(DirectionsCriteria.OVERVIEW_FULL)
+                .profile(DirectionsCriteria.PROFILE_DRIVING)
+                .steps(true)
+                .build();
+        List<MissionStep> missionSteps = new ArrayList<>();
+        try {
+
+            Response<DirectionsResponse> response = request.executeCall();
+
+            if (response.body() == null || response.body().routes().isEmpty()) {
+                log.warn("No routes found; check access token, rights and coordinates");
+            } else {
+                Optional<List<RouteLeg>> legs = Optional.ofNullable(response.body().routes().get(0).legs());
+                legs.orElse(Collections.emptyList()).stream().flatMap(r -> Optional.ofNullable(r.steps()).orElse(Collections.emptyList()).stream())
+                .map(l -> {
+                    Point p = l.maneuver().location();
+                    MissionStep.Builder builder = MissionStep.builder(BigDecimal.valueOf(p.latitude()).setScale(5, RoundingMode.HALF_UP),
+                            BigDecimal.valueOf(p.longitude()).setScale(5, RoundingMode.HALF_UP));
+                    if ("arrive".equalsIgnoreCase(l.maneuver().type())) {
+                        Optional<MissionStep> step = missionSteps.stream().filter(MissionStep::isWayPoint).findFirst();
+                        if (step.isEmpty()) {
+                            builder.wayPoint(true);
+                        } else {
+                            builder.destination(true);
+                        }
+                    }
+                    return builder.build();
+                }).forEach(missionSteps::add);
+            }
+        } catch (IOException e) {
+            log.error("Exception while calling MapBox API", e);
+        }
+        return missionSteps;
     }
 
 }
---
+++ src/test/java/com/redhat/emergency/response/map/RoutePlannerTest.java
@@ -0,0 +1,124 @@
+package com.redhat.emergency.response.map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.redhat.emergency.response.model.Location;
+import com.redhat.emergency.response.model.MissionStep;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RoutePlannerTest {
+
+    RoutePlanner routePlanner;
+
+    private WireMockServer mockServer;
+
+    @BeforeEach
+    void setup() {
+        mockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
+        mockServer.start();
+        routePlanner = new RoutePlanner();
+        setField(routePlanner, "mapboxUrl", "http://localhost:" + mockServer.port());
+        setField(routePlanner, "accessToken", "pk.replaceme");
+    }
+
+    @AfterEach
+    void tearDown() {
+        mockServer.stop();
+    }
+
+    @Test
+    void testRoutePlanner() throws IOException {
+
+        String url = "/directions/v5/mapbox/driving/-77.90999,34.18323;-77.84856,34.18408;-77.949,34.1706?access_token=pk.replaceme&geometries=polyline6&overview=full&steps=true";
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("mapbox/directions.json");
+        String json = IOUtils.toString(is, Charset.defaultCharset());
+        mockServer.stubFor(get(urlEqualTo(url))
+                .willReturn(aResponse().withStatus(200).withBody(json)));
+        Location start = Location.of(new BigDecimal("34.18323"), new BigDecimal("-77.90999"));
+        Location destination = Location.of(new BigDecimal("34.1706"), new BigDecimal("-77.949"));
+        Location waypoint = Location.of(new BigDecimal("34.18408"), new BigDecimal("-77.84856"));
+        List<MissionStep> steps = routePlanner.getDirections(start, destination, waypoint);
+
+        assertThat(steps, notNullValue());
+        assertThat(steps.size(), equalTo(22));
+        assertThat(steps.get(0).isWayPoint(), equalTo(false));
+        assertThat(steps.get(0).isDestination(), equalTo(false));
+        assertThat(steps.get(13).isWayPoint(), equalTo(true));
+        assertThat(steps.get(13).isDestination(), equalTo(false));
+        assertThat(steps.get(21).isWayPoint(), equalTo(false));
+        assertThat(steps.get(21).isDestination(), equalTo(true));
+    }
+
+    private void setField(Object targetObject, String name, Object value) {
+
+        Class<?> targetClass = targetObject.getClass();
+        Field field = findField(targetClass, name);
+        if (field == null) {
+            throw new IllegalArgumentException(String.format(
+                    "Could not find field '%s' on %s", name, targetClass));
+        }
+        makeAccessible(field, targetObject);
+        setField(field, targetObject, value);
+    }
+
+    private Field findField(Class<?> clazz, String name) {
+        Class<?> searchType = clazz;
+        while (Object.class != searchType && searchType != null) {
+            Field[] fields = getDeclaredFields(searchType);
+            for (Field field : fields) {
+                if (name.equals(field.getName())) {
+                    return field;
+                }
+            }
+            searchType = searchType.getSuperclass();
+        }
+        return null;
+    }
+
+    private Field[] getDeclaredFields(Class<?> clazz) {
+        try {
+            return clazz.getDeclaredFields();
+        }
+        catch (Throwable ex) {
+            throw new IllegalStateException("Failed to introspect Class [" + clazz.getName() +
+                    "] from ClassLoader [" + clazz.getClassLoader() + "]", ex);
+        }
+    }
+
+    private void makeAccessible(Field field, Object object) {
+        if ((!Modifier.isPublic(field.getModifiers()) ||
+                !Modifier.isPublic(field.getDeclaringClass().getModifiers()) ||
+                Modifier.isFinal(field.getModifiers())) && !field.canAccess(object)) {
+            field.setAccessible(true);
+        }
+    }
+
+    private void setField(Field field, Object target, Object value) {
+        try {
+            field.set(target, value);
+        }
+        catch (IllegalAccessException ex) {
+            throw new IllegalStateException(
+                    "Unexpected reflection exception - " + ex.getClass().getName() + ": " + ex.getMessage());
+        }
+    }
+
+}
